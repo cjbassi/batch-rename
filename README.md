35cce84f8c31a0c51ca11feaae64d24921efc0cc
Caleb Bassi <calebjbassi@gmail.com>
2019-04-27 18:14:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# batch-rename
+
+Quickly rename a batch of files in your default text editor.
+
+## Installation
+
+### Prebuilt binaries:
+
+Note: (currently only a binary for Linux-x86_64 is available)
+
+Run the following to download the correct binary for your system from the releases tab into `$CARGO_HOME/bin`, courtesy of [japaric/trust](https://github.com/japaric/trust):
+
+```bash
+bash <(curl -LSfs https://japaric.github.io/trust/install.sh) \
+  -f --git cjbassi/batch-rename
+```
+
+### From source:
+
+```bash
+cargo install --git https://github.com/cjbassi/batch-rename
+```
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::env;
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::Command;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+pub struct Args {
+    pub directory: Option<PathBuf>,
+
+    #[structopt(short = "f", long = "force")]
+    pub force: bool,
+
+    #[structopt(short = "v", long = "verbose")]
+    pub verbose: bool,
+}
+
+fn main() {
+    let args = Args::from_args();
+
+    let temp_dir = tempfile::tempdir().unwrap();
+    let temp_filepath = temp_dir.path().join("tempfile");
+    let mut temp_file = fs::File::create(&temp_filepath).unwrap();
+    if let Some(dir) = args.directory {
+        env::set_current_dir(dir).unwrap();
+    }
+    let cwd = env::current_dir().unwrap();
+    for dir_entry in fs::read_dir(cwd).unwrap() {
+        let filepath = dir_entry.unwrap().path();
+        let filename = filepath.file_name().unwrap().to_string_lossy();
+        writeln!(temp_file, "{} {}", filename, filename).unwrap();
+    }
+
+    let editor = env::var("EDITOR").unwrap();
+    let status = Command::new(editor)
+        .args(&[temp_filepath.to_string_lossy().to_string()])
+        .status()
+        .unwrap();
+    if !status.success() {
+        return;
+    }
+
+    for line in fs::read_to_string(temp_filepath).unwrap().lines() {
+        let filenames = line.split_whitespace().collect::<Vec<&str>>();
+        let from = filenames[0];
+        let to = filenames[1];
+        fs::rename(from, to).unwrap();
+        if args.verbose {
+            println!("renamed '{}' -> '{}'", from, to);
+        }
+    }
+}

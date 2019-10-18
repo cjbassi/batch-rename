4f6b961e4f8e15532ea8f7812454b3069d9b531d
Caleb Bassi <calebjbassi@gmail.com>
2019-10-17 19:33:37 -0700

    Switch to anyhow for error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-#[macro_use]
-mod errors;
-
 use std::env;
 use std::fs;
 use std::io::Write;
 use std::path::PathBuf;
 use std::process::{exit, Command};
 
+use anyhow::{anyhow, bail, ensure, Context, Result};
 use structopt::StructOpt;
-
-use errors::UnwrapOrExit;
 
 #[derive(StructOpt, Debug)]
 pub struct Args {
@@ -24,17 +20,16 @@ pub struct Args {
     pub verbose: bool,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::from_args();
 
-    let temp_dir = tempfile::tempdir().unwrap_or_exit("create temp dir");
+    let temp_dir = tempfile::tempdir().context("failed to create a temp dir")?;
     let temp_filepath = temp_dir.path().join("tempfile");
     let mut temp_file = fs::File::create(&temp_filepath)
-        .unwrap_or_exit(&format!("create temp file {}", &temp_filepath.display()));
+        .with_context(|| format!("failed to create temp file {}", &temp_filepath.display()))?;
 
     let mut filenames = if args.paths.is_empty() {
-        fs::read_dir(env::current_dir().unwrap())
-            .unwrap()
+        fs::read_dir(env::current_dir()?)?
             .map(|entry| PathBuf::from(entry.unwrap().path().file_name().unwrap()))
             .collect()
     } else {
@@ -42,28 +37,34 @@ fn main() {
     };
 
     filenames.sort();
-    filenames.iter().for_each(|filename| {
-        writeln!(temp_file, "{}", filename.display())
-            .unwrap_or_exit(&format!("write to file {}", temp_filepath.display()));
-    });
+    writeln!(
+        temp_file,
+        "{}",
+        filenames
+            .iter()
+            .map(|pathbuf| pathbuf.to_string_lossy().to_string())
+            .collect::<Vec<String>>()
+            .join("\n")
+    )
+    .with_context(|| format!("failed to write to file {}", temp_filepath.display()))?;
 
-    let editor = env::var("EDITOR").unwrap_or_exit("read EDITOR env variable");
+    let editor = env::var("EDITOR").context("failed to read EDITOR env variable")?;
+    ensure!(editor != "", "EDITOR is set to empty string");
     let status = Command::new(&editor)
         .args(&[temp_filepath.to_string_lossy().to_string()])
         .status()
-        .unwrap_or_exit(&format!("execute {}", editor));
+        .with_context(|| format!("failed to execute {}", editor))?;
     if !status.success() {
         exit(1);
     }
 
     let new_filenames: Vec<PathBuf> = fs::read_to_string(&temp_filepath)
-        .unwrap_or_exit(&format!("read temp_file {}", temp_filepath.display()))
+        .with_context(|| format!("failed to read temp file {}", temp_filepath.display()))?
         .lines()
         .map(PathBuf::from)
         .collect();
     if filenames.len() != new_filenames.len() {
-        eprintln!("error: incorrect number of file names");
-        exit(1);
+        bail!("incorrect number of file names");
     }
 
     filenames
@@ -73,21 +74,32 @@ fn main() {
         .for_each(|(from, to)| {
             if to.exists() && !args.force {
                 eprintln!(
-                    "error: failed to rename '{}' to '{}': file already exists",
-                    from.display(),
-                    to.display()
+                    "{}",
+                    anyhow!(
+                        "failed to rename '{}' to '{}': file already exists",
+                        from.display(),
+                        to.display()
+                    )
                 );
             } else {
-                match fs::rename(from, to) {
+                match fs::rename(from, to).with_context(|| {
+                    anyhow!(
+                        "failed to rename '{}' to '{}'",
+                        from.display(),
+                        to.display()
+                    )
+                }) {
                     Ok(()) => {
                         if args.verbose {
                             println!("renamed '{}' -> '{}'", from.display(), to.display());
                         }
                     }
                     Err(e) => {
-                        print_error!("rename '{}' to '{}'", from.display(), to.display(), e);
+                        eprintln!("{}", e);
                     }
                 }
             }
         });
+
+    Ok(())
 }
